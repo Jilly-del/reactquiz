36150aecf465edb8c7cce127af3766422bebd98d
Akanni Ajibola <jilboy72@gmail.com>
2025-01-07 22:39:09 +0100

    few updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import "./index.css";
 import Main from "./Mainn";
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        statuz: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        statuz: "error",
+      };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
+const initialState = {
+  questions: [],
+  // loading,error,ready,active,finished
+  statuz: "loading",
+};
 export default function App() {
+  const [state, dispact] = useReducer(reducer, initialState);
+
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => dispact({ type: "dataReceived", payload: data }))
+      .catch((err) => dispact({ type: "dataFailed" }));
   }, []);
+
   return (
     <div className="app">
       <Header />
